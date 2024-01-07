85e59c5ddb97dad1f7107f59d41a001be9b02050
Kalle Hahtela <khahtela@gmail.com>
2024-01-07 22:00:28 +0200

    started a react native app for IOS, Android & Web.

---
+++ Fibaste/App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F0F0C9',
+    alignItems: 'center',
+    justifyContent: 'center',
+    textTransform: 'uppercase',
+  },
+});
